b02a431f86c3d79a1d5a90a057828450cb42222c
khrystoliubov <v.khristoliubov@red-beat.com>
2024-08-31 12:00:43 +0300

    added flapStrength&set it in Unity

--- My project/Assets/BirdScript.cs
+++ My project/Assets/BirdScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigibody;
+    public float flapStrength;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,10 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ()
-        myRigibody.velocity = Vector2.up * 10;
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigibody.velocity = Vector2.up * flapStrength;
+        }
+
     }
 }

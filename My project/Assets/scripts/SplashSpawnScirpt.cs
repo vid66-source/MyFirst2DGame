163a78252400ce6033af74aa8bcfec6390a3ab3b
khrystoliubov <v.khristoliubov@red-beat.com>
2024-09-04 18:31:36 +0300

    Scripts updates(splash&pipespawn)

--- My project/Assets/scripts/SplashSpawnScirpt.cs
+++ My project/Assets/scripts/SplashSpawnScirpt.cs
@@ -7,15 +7,15 @@ public class SplashSpawnScirpt : MonoBehaviour
     public BirdScript birdScript;
     public GameObject splashPrefab;
     public GameObject[] markOnScreen;
-    public int minSpawnCountSplash = 2;
-    public int maxSpawnCountSplash = 5;
-    public int minSpawnCountDrops = 6;
-    public int maxSpawnCountDrops = 12;
+    public int minSpawnCountSplash;
+    public int maxSpawnCountSplash;
+    public int minSpawnCountDrops;
+    public int maxSpawnCountDrops;
     private bool hasSpawned = false;
     public string sortingLayerName = "Foreground";
     public int sortingOrder = 1;
-    public float minScale = 1.5f; 
-    public float maxScale = 3f;
+    public float minScale;
+    public float maxScale;
     // Start is called before the first frame update
     void Start()
     {
@@ -38,14 +38,15 @@ public class SplashSpawnScirpt : MonoBehaviour
 
     void SpawnMultipleSplash()
     {
-        int spawnCount = Random.Range(minSpawnCountSplash, maxSpawnCountSplash + 1); // Randomly choose number of spawns
+        int spawnCountSplash = Random.Range(minSpawnCountSplash, maxSpawnCountSplash + 1); // Randomly choose number of spawns
 
         Camera mainCamera = Camera.main;
 
         float screenWidth = mainCamera.orthographicSize * mainCamera.aspect;
+
         float screenHeight = mainCamera.orthographicSize;
 
-        for (int i = 0; i < spawnCount; i++)
+        for (int i = 0; i < spawnCountSplash; i++)
         {
             // Generate a random position within the screen bounds
             Vector3 spawnPosition = new Vector3(Random.Range(-screenWidth, screenWidth),Random.Range(-screenHeight, screenHeight),0);
@@ -54,7 +55,7 @@ public class SplashSpawnScirpt : MonoBehaviour
         }
     }
     void SpawnMultipleDrops()
-     {
+    {
         // Get the camera component
         Camera mainCamera = Camera.main;
 
@@ -63,10 +64,10 @@ public class SplashSpawnScirpt : MonoBehaviour
         float screenHeight = mainCamera.orthographicSize;
 
         // Determine how many prefabs to spawn
-        int spawnCount = Random.Range(minSpawnCountDrops, maxSpawnCountDrops + 1);
+        int spawnCountDrops = Random.Range(minSpawnCountDrops, maxSpawnCountDrops + 1);
 
         // Spawn the prefabs
-        for (int i = 0; i < spawnCount; i++)
+        for (int i = 0; i < spawnCountDrops; i++)
         {
             // Generate a random position within the camera's view
             Vector3 spawnPosition = new Vector3(Random.Range(-screenWidth, screenWidth),Random.Range(-screenHeight, screenHeight),0);

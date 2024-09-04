5f67ad572e307932b3efb4a25587f0469cda07b4
khrystoliubov <v.khristoliubov@red-beat.com>
2024-09-04 23:17:59 +0300

    newSpriteForBird

--- My project/Assets/scripts/BirdScript.cs
+++ My project/Assets/scripts/BirdScript.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigibody;
+    private SpriteRenderer spriteRenderer;
+    public Sprite firstBirdSprite;
+    public Sprite secondbirdSprite;
     public float flapStrength;
     public LogicScript logic;
     public bool birdIsAlive = true;
     // Start is called before the first frame update
+    
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = firstBirdSprite;
+    }
+
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        
     }
 
     // Update is called once per frame
@@ -33,6 +44,17 @@ public class BirdScript : MonoBehaviour
         {
             birdIsAlive = false;
             logic.gameOver();
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Change to the pressed sprite
+            spriteRenderer.sprite = firstBirdSprite;
+        }
+        else
+        {
+            // Revert back to the default sprite when the button is released
+            spriteRenderer.sprite = secondbirdSprite;
         }
 
     }
